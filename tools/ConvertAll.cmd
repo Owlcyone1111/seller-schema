f2bae4acbfe72b770375b56bf51dcee76a6dcc12
Nick DeAngelo <nick@jet.com>
2014-06-27 16:07:42 -0400

    bugifxes

--- tools/ConvertAll.cmd
+++ tools/ConvertAll.cmd
@@ -1,1 +1,1 @@
-fsi.exe --exec --nologo BatchConvert.fsx ..
+"C:\Program Files (x86)\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi.exe" --exec --nologo BatchConvert.fsx ..
--- tools/ValidateAll.cmd
+++ tools/ValidateAll.cmd
@@ -1,1 +1,1 @@
-fsi.exe --exec --nologo ValidateSchemas.fsx ..
+"C:\Program Files (x86)\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi.exe" --exec --nologo ValidateSchemas.fsx .. 
