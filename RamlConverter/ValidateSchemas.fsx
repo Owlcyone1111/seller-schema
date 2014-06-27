aadb94b1d81e07e59b493bc34337dbb22eb169e7
dsevastianov <dsevastianov@gmail.com>
2014-06-27 13:35:01 -0400

    Normalizing tools

--- RamlConverter/ValidateSchemas.fsx
+++ RamlConverter/ValidateSchemas.fsx
@@ -4,35 +4,36 @@
 open Json
 open System.IO
 
-let source = 
-    if fsi.CommandLineArgs.Length < 2
-    then  @"c:\users\dmitry\source\repos\seller-schema"
-    else fsi.CommandLineArgs.[1]
-
-let files = Directory.EnumerateFiles(source, "*.json", SearchOption.AllDirectories )
-
-let (|Suffix|_|) (suffix:string) (s : string) = match s.LastIndexOf(suffix) with | -1 -> None | p -> Some (s.Substring(0, p), s)
-
 type JsonType = | Schema | Example
 
-let getError = function | Some(Error s) -> s | _ -> []
+if fsi.CommandLineArgs.Length < 2
+then  
+    printf "Please provide raml file path"
+else
+    let source = fsi.CommandLineArgs.[1]
 
-files
-|> Seq.choose(function | Suffix ".example.json" s -> Some (fst s,(Example,snd s)) |  Suffix ".schema.json" s -> Some (fst s, (Schema, snd s)) | _-> None )
-|> Seq.groupBy fst
-|> Seq.map (fun (key,s) ->
-    let pair = s |> Seq.map snd |> Map.ofSeq 
-    let schemaResult = pair.TryFind Schema |> Option.map (fun name -> File.ReadAllText(name) |> parseSchema name) 
-    let schema = schemaResult |> Option.bind(function | Validated s -> Some s | _ -> None)
-    [
-        yield! getError schemaResult
-        yield! pair.TryFind Example 
-               |> Option.map (fun name -> File.ReadAllText(name) |> parseJson schema name) 
-               |> getError
-    ]
-    )
-|> Seq.concat
-|> Seq.iter (printfn "%A")
+    let files = Directory.EnumerateFiles(source, "*.json", SearchOption.AllDirectories )
+
+    let (|Suffix|_|) (suffix:string) (s : string) = match s.LastIndexOf(suffix) with | -1 -> None | p -> Some (s.Substring(0, p), s)
+
+    let getError = function | Some(Error s) -> s | _ -> []
+
+    files
+    |> Seq.choose(function | Suffix ".example.json" s -> Some (fst s,(Example,snd s)) |  Suffix ".schema.json" s -> Some (fst s, (Schema, snd s)) | _-> None )
+    |> Seq.groupBy fst
+    |> Seq.map (fun (key,s) ->
+        let pair = s |> Seq.map snd |> Map.ofSeq 
+        let schemaResult = pair.TryFind Schema |> Option.map (fun name -> File.ReadAllText(name) |> parseSchema name) 
+        let schema = schemaResult |> Option.bind(function | Validated s -> Some s | _ -> None)
+        [
+            yield! getError schemaResult
+            yield! pair.TryFind Example 
+                   |> Option.map (fun name -> File.ReadAllText(name) |> parseJson schema name) 
+                   |> getError
+        ]
+        )
+    |> Seq.concat
+    |> Seq.iter (printfn "%A")
 
     
 
