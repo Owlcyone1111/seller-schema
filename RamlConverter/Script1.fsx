4c4dc128bfad651f29283dd6750462cb034d2784
dsevastianov <dmitry@jet.com>
2014-10-23 13:06:47 -0400

    Validation for type on json objects in schema

--- RamlConverter/Script1.fsx
+++ RamlConverter/Script1.fsx
@@ -1,12 +1,14 @@
-﻿#r @"..\tools\RamlConverter.dll"
+﻿#I "../tools/"
+#r @"RamlConverter.dll"
 
 open System.IO
 
-let ast = RamlParser.parseAst @"C:\Users\Dmitry\Source\Repos\seller-schema\orders\orders.raml"
+let path = @"C:\Users\Dmitry\Source\Repos\seller-schema\settlement\settlement.raml"
+let ast = RamlParser.parseAst path
     
 match  ast with
 | Validated api -> 
-    WadlGen.toXml api @"C:\Users\Dmitry\Source\Repos\seller-schema\orders\orders.wadl"
+    WadlGen.toXml api (path.Replace(".raml", ".wadl"))
 | Error errors -> 
     printfn "Can't generate, errors encountered:" 
     errors |>  Seq.iter (printfn "%s")
