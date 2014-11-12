5e4ac54741b571720aec27e33ca2d0bd8bd01972
dsevastianov <dmitry@jet.com>
2014-11-12 12:56:37 -0500

    Upgrading to latest marvel

--- RamlConverter/Script1.fsx
+++ RamlConverter/Script1.fsx
@@ -1,38 +1,14 @@
 ﻿#r @"../tools/Marvel.Fsharp.dll"
 #r @"../tools/Marvel.Json.Fsharp.dll"
-#r @"../tools/RamlConverter.dll"
 
 open Marvel
-open Marvel.Validation
 open FSharp.Data
-open FSharp.Data.JsonExtensions
-
-let fvalidate test err e = if not (test e) then fail [err e] else puree ()
-let validate test err = fvalidate (konst test) (konst err) ()
-let optionValidate x = Option.map x >> Option.isNullLazy puree 
-let validateOptional test err = optionValidate (fvalidate test err)
-let collectErrors = traverse id >> function Success _ -> puree () | Failure e -> fail e
-
-let rec Validate path (el,schema) json = 
-    let err e = sprintf "%s. Path: %s" e path
-    fvalidate (fun _ -> el.required) (sprintf "Required property is missing"
-    <* match schema, json with
-        | SchemaType.String s, JsonValue.String str -> 
-            puree ()
-            <* validateOptional (Seq.contains str) (String.concat(",") >> sprintf "Invalid value, should be one of [%s]" >> err) el.enum
-            
-        | SchemaType.Object s, JsonValue.Record properties -> 
-            let props = properties |> Map.ofArray
-            let propFinder name = props |> Map.tryFind name |> Option.isNull JsonValue.Null 
-            s.properties 
-            |> Seq.map (fun (propName, s) -> propFinder propName |> Validate (sprintf "%s/%s" path propName) s) 
-            |> collectErrors            
-        | x, y -> fail [sprintf "Expected %A, got %A" x y]
-
+open FSharp.Data.JsonSchema
+open Marvel.Validation
 
 let json =  JsonValue.Parse """
 {
-    "foo": { "bar": "foo" }
+    "foo": { "bar": "foo", "foo" : 3, "arr" : ["foo", "bar"] }
 }
 """
 
@@ -47,6 +23,18 @@ let schema = JsonSchema.Parse  <| JsonValue.Parse """
             "properties" : {
                 "bar" : {
                     "$ref" : "#/definitions/bar"
+                },
+                "foo" : {
+                    "type" : "integer",
+                    "exclusiveMaximum" : 3,
+                    "exclusiveMinimum" : 1
+                },
+                "arr" : {
+                    "type" : "array",
+                    "required" : true,
+                    "minItems" : 1,
+                    "maxItems" : 2,
+                    "items" : { "$ref" : "#/definitions/bar"}
                 }
             }
         }
@@ -56,9 +44,11 @@ let schema = JsonSchema.Parse  <| JsonValue.Parse """
             "type" : "string",
             "enum" : [ "foo", "bar"],
             "required" : true
-        }
+        }        
     }
 }
 """ 
 
-Validate "" schema json
+match Validate schema json with
+| Success _ -> printfn "Success!"
+| Failure e -> e |> Seq.iter (printfn "%s")
